79f518ac47b29cdbc341ef66fd21d2470d4a6673
Andrey Zhezlov <zhezlov@gmail.com>
2021-04-28 14:10:52 +0400

    ready for deploy to heroku

--- src/main/java/org/zhezlov/documentarchive/Utils/FilePathUtil.java
+++ src/main/java/org/zhezlov/documentarchive/Utils/FilePathUtil.java
@@ -4,6 +4,9 @@ package org.zhezlov.documentarchive.Utils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.ServletContext;
+import java.io.File;
+
 @Configuration
 public class FilePathUtil {
 
@@ -16,20 +19,19 @@ public class FilePathUtil {
     @Value("${path.file.warehouse}")
     private String pathFileWarehouse;
 
+    private ServletContext context;       //toDo закомментировал чтобы получилось запустить тесты. Раскомментировать перед деплоем
 
-//    private ServletContext context;       //toDo закомментировал чтобы получилось запустить тесты. Раскомментировать перед деплоем
-//
-//    public FilePathUtil(ServletContext context) {
-//        this.context = context;
-//    }
+    public FilePathUtil(ServletContext context) {
+        this.context = context;
+    }
 
     public String getFolderPath() {
 
-//        if (saveFileInProjectDirectory) {         //toDo закомментировал чтобы получилось запустить тесты. Раскомментировать перед деплоем
-//            if (subFolder != null && !subFolder.isEmpty()) {
-//                return context.getRealPath("") + File.separator + subFolder;
-//            } else return context.getRealPath("");
-//        } else
+        if (saveFileInProjectDirectory) {         //toDo закомментировал чтобы получилось запустить тесты. Раскомментировать перед деплоем
+            if (subFolder != null && !subFolder.isEmpty()) {
+                return context.getRealPath("") + File.separator + subFolder;
+            } else return context.getRealPath("");
+        } else
             return pathFileWarehouse;
 
     }
